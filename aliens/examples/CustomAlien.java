9e91763371dfaeec4ee63c315d0e69a92547eae2
gunnarmein <gmein@eastsideprep.org>
2016-04-27 10:12:13 -0700

    cleaned up custom alien source

--- aliens/examples/CustomAlien.java
+++ aliens/examples/CustomAlien.java
@@ -1,53 +1,41 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Example empty custom alien for free distribution and sharing
  */
+
 package customalien;
 
 import alieninterfaces.*;
 
-/**
- *
- * @author gunnar
- */
 public class CustomAlien implements Alien{
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
 
     @Override
     public void init(Context ctx, int id, int parent, String message) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+        throw new UnsupportedOperationException("Not supported yet."); 
+    } 
+    
     @Override
     public void communicate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
     @Override
     public void receive(String[] messages) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
     @Override
     public MoveDir getMove() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
     @Override
     public Action getAction() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void beThoughtful() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
 }
